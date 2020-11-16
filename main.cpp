ad57a8ab16864c9d32634acf9db4938d8e13ad98
Karol0703 <56909943+Karol0703@users.noreply.github.com>
2020-11-16 09:19:54 +0100

    58.1

--- main.cpp
+++ main.cpp
@@ -1,35 +1,56 @@
-#include <stdio.h>
-#include <iostream>
-#include <bitset>
-using namespace std;
+# include  < iostream >
+# include  < fstream >
+# include  < stdio.h >
+# include  < stdlib.h >
+# include  < string.h >
 
-int main() {
-    int l1;
-    int l2;
-	
-	cout<<"liczba 1:";
-    cin>>l1;
-    	cout<<"liczba 2:";
-   		 cin>>l2;
+using  namespace  std ;
 
-    	cout<<l1<<" "<<endl<<bitset<8>(l1).to_string()<<endl<<(l1)<<endl<<endl;
-
-   		 	cout<<l2<<" "<<bitset<8>(l2).to_string()<<endl<<(l2)<<endl<<endl;
-
-   				 cout<<l1<<"|"<<l2<<" "<<bitset<8>(l1|l2).to_string()<<endl<<(l1|l2)<<endl<<endl;
-
-    				cout<<l1<<"&"<<l2<<" "<<bitset<8>(l1&l2).to_string()<<endl<<(l1&l2)<<endl<<endl;
-
-    					cout<<l1<<"^"<<l2<<" "<<bitset<8>(l1^l2).to_string()<<endl<<(l1^l2)<<endl<<endl;
-
-    						cout<<"~"<<l1<<" "<<bitset<8>(~l1).to_string()<<endl<<(~l1)<<endl<<endl;
-
-    							cout<<"~"<<l2<<" "<<bitset<8>(~l2).to_string()<<endl<<(~l1)<<endl<<endl;
-
-    								cout<<l1<<"<<=4 "<<" "<<bitset<8>(l1<<=4).to_string()<<endl<<(l1<<=4)<<endl<<endl;
-
-    									cout<<l2<<">>=1 "<<" "<<bitset<8>(l2>>=1).to_string()<<endl<<(l1>>=1);
-    
-    //by Karol Ptak
-    return 0;
+d³ugie  minimumZPliku (string nazwaPliku, int system)
+{
+  
+  plik ifstream;
+  
+	char * endptr;
+ 	d³uga liczba, min;
+ 	string liczba1, liczba2; 
+ 	plik. open (nazwaPliku);
+ 	
+ 	bool pierwszy = prawda ;
+ 
+ if (plik. good ())  
+     while (! file. eof ())        
+     {
+               plik >> liczba1 >> liczba2;  
+               liczba =   strtol (liczba2. c_str (), & endptr, system );
+              
+               if (pierwszy)
+               {
+                            min = liczba;
+                            pierwszy = false ;
+               }
+               jeszcze 
+                  if (liczba <min)
+                       min = liczba; 
+     }
+  plik. close ();
+  powrót min;
 }
+
+int  main ( int argc, char ** argv) {
+	
+	ofstream fileOut;
+	fileOut. open ( " wynik.txt " );
+	
+	long min1 = minimumZPliku ( " dane_systemy1.txt " , 2 );
+  	long min2 = minimumZPliku ( " dane_systemy2.txt " , 4 );
+  	long min3 = minimumZPliku ( " dane_systemy3.txt " , 8 );
+  	
+  	fileOut << " Plik pierwszy min: " << min1 << " \ n " ;
+  	fileOut << " Plik drugi min: " << min2 << " \ n " ;
+  	fileOut << " Plik trzeci min: " << min3 << " \ n " ;
+	
+	fileOut. close ();
+	return  0 ;
+}
+
